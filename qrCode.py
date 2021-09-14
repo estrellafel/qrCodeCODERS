c9f35fa4d24ab8fd7193eab9cc064200aac7f6e0
estrellafel <felixestrella7@gmail.com>
2021-09-13 20:58:45 -0500

    Added a qr code for laso
    Signed-off-by: estrellafel <felixestrella7@gmail.com>

--- qrCode.py
+++ qrCode.py
@@ -1,5 +1,5 @@
 import pyqrcode
 import png
 
-url = pyqrcode.create('https://docs.google.com/forms/d/e/1FAIpQLScKr3lXqOtP6clCB_89muSAcHkphVmHg4U4AAkD3iZKL_EyIg/viewform?usp=sf_link')
-url.png('involvementFest2021.png', scale = 10)
+url = pyqrcode.create('https://docs.google.com/forms/d/1X8HJcUJfFMgQxCVehBnouqHy9AbHf0urFbAFLmFxHPU/edit')
+url.png('LASOinvolvementFest2021.png', scale = 10)
