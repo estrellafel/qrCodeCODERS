db17219b7a6e119737863ddacad5d08b594100eb
estrellafel <felixestrella7@gmail.com>
2021-09-13 21:03:26 -0500

    fixed name
    Signed-off-by: estrellafel <felixestrella7@gmail.com>

--- qrCode.py
+++ qrCode.py
@@ -2,4 +2,4 @@ import pyqrcode
 import png
 
 url = pyqrcode.create('https://docs.google.com/forms/d/1X8HJcUJfFMgQxCVehBnouqHy9AbHf0urFbAFLmFxHPU/edit')
-url.png('LASOinvolvementFest2021.png', scale = 10)
+url.png('involvementFest2021.png', scale = 10)
